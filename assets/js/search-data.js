37c87f8a53fcd0c8975ca9fd641fbd8d0748ec0b
jeetv <jeet.vora2010@gmail.com>
2025-06-27 18:00:31 +0000

    Deploying to gh-pages from @ jeetv/jeetv.github.io@27782a53bf6245367af3d1cdb99d2331342128be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-video",
+          title: 'video',
+          description: "this is what included videos could look like",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
